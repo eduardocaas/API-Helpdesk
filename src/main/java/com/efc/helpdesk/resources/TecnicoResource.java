7071fd8f71d1feeefb84c122326c884670d6b868
Eduardo Castro <eduardofc.dev@gmail.com>
2022-12-19 17:57:36 -0300

    DTO criado para perfil tecnico

--- src/main/java/com/efc/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/efc/helpdesk/resources/TecnicoResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.efc.helpdesk.domain.Tecnico;
+import com.efc.helpdesk.domain.dtos.TecnicoDTO;
 import com.efc.helpdesk.services.TecnicoService;
 
 // Intercepta requisições HTTP e envia para camada de service -> que faz conexão com o banco
@@ -22,10 +23,10 @@ public class TecnicoResource {
 	private TecnicoService service;
 	
 	@GetMapping(value = "/{id}") // Recebe uma PathVariable
-	public ResponseEntity<Tecnico> findById(@PathVariable Integer id){ // ResponseEntity -> representa toda resposta HTTP
+	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id){ // ResponseEntity -> representa toda resposta HTTP
 		
 		Tecnico obj = service.findById(id);
-		return ResponseEntity.ok().body(obj); // Retorna o objeto no corpo da resposta
+		return ResponseEntity.ok().body(new TecnicoDTO(obj)); // Retorna o objeto no corpo da resposta
 	}
 	
 }
