b1712e57d53e27554aeeaa9a7b4a5cbbc9d97e88
Eduardo Castro <eduardofc.dev@gmail.com>
2022-12-23 20:09:28 -0300

    endpoint create para técnico

--- src/main/java/com/efc/helpdesk/domain/Tecnico.java
+++ src/main/java/com/efc/helpdesk/domain/Tecnico.java
@@ -2,7 +2,9 @@ package com.efc.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import com.efc.helpdesk.domain.dtos.TecnicoDTO;
 import com.efc.helpdesk.domain.enums.Perfil;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -26,6 +28,17 @@ public class Tecnico extends Pessoa {
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
 		addPerfil(Perfil.TECNICO);
+	}
+	
+	public Tecnico(TecnicoDTO obj) { // Construtor para TecnicoDTO
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
 	}
 
 	public List<Chamado> getChamados() {
