3d92e44ded3eba543250f81870eaee94153f363b
Eduardo Castro <eduardofc.dev@gmail.com>
2022-12-29 21:12:08 -0300

    Update DBService.java

--- src/main/java/com/efc/helpdesk/services/DBService.java
+++ src/main/java/com/efc/helpdesk/services/DBService.java
@@ -33,18 +33,20 @@ public class DBService {
 		tec1.addPerfil(Perfil.ADMIN);
 		Tecnico tec2 = new Tecnico(null, "Ciclano", "998.332.660-45" , "ciclano@email.com", "OIhQwI");
 		Tecnico tec3 = new Tecnico(null, "Maria", "621.433.020-10" , "maria@email.com", "a2KDCm");
+		Tecnico tec4 = new Tecnico(null, "Ana", "031.196.700-06" , "ana@email.com", "op4lks");
 		
 		
 		Cliente cli1 = new Cliente(null, "Beltrano", "298.070.940-97", "beltrano@email.com", "bel123trano");
 		Cliente cli2 = new Cliente(null, "Jorge", "748.230.360-00", "jorge@email.com", "zobtk0");
 		Cliente cli3 = new Cliente(null, "Pedro", "209.380.940-09", "pedro@email.com", "y6wdy5");
+		Cliente cli4 = new Cliente(null, "Luis", "421.108.470-42", "luis@email.com", "vh7d34");
 		
 		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro chamado", tec1, cli1);
 		Chamado c2 = new Chamado(null, Prioridade.ALTA, Status.ANDAMENTO, "Chamado 02", "Segundo chamado", tec2, cli3);
 		Chamado c3 = new Chamado(null, Prioridade.BAIXA, Status.ABERTO, "Chamado 03", "Terceiro chamado", tec3, cli2);
 		
-		tecnicoRepository.saveAll(Arrays.asList(tec1, tec2, tec3));
-		clienteRepository.saveAll(Arrays.asList(cli1, cli2, cli3));
+		tecnicoRepository.saveAll(Arrays.asList(tec1, tec2, tec3, tec4));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2, cli3, cli4));
 		chamadoRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 	}
