bb153e4ad4c8fb9a8ee0e3f01683aca0837b2e48
Eduardo Castro <eduardofc.dev@gmail.com>
2023-01-10 17:09:36 -0300

    Validação de token e manipulação de claims

--- src/main/java/com/efc/helpdesk/security/JWTUtil.java
+++ src/main/java/com/efc/helpdesk/security/JWTUtil.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -24,6 +25,36 @@ public class JWTUtil {
 					.setExpiration(new Date(System.currentTimeMillis() + expiration)) // Horário atual + expiração
 					.signWith(SignatureAlgorithm.HS512, secret.getBytes()) // Algoritimo de encriptação + Segredo (String para bytes)
 					.compact(); // Compactação -> performance
+	}
+
+	public boolean tokenValido(String token) {
+		Claims claims = getClaims(token); // Claims -> informações da entidade
+		if(claims != null) {
+			String username = claims.getSubject(); // Subject - campos do usuário
+			Date expirationDate = claims.getExpiration(); // Campo de expiração
+			Date now = new Date(System.currentTimeMillis());
+			
+			if(username != null && expirationDate != null && now.before(expirationDate)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private Claims getClaims(String token) {
+		try {
+			return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody(); // Pega o corpo do token
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	public String getUsername(String token) {
+		Claims claims = getClaims(token);
+		if(claims != null) {
+			return claims.getSubject(); // Retorna o subject do usuário
+		}
+		return null;
 	}
 	
 }
