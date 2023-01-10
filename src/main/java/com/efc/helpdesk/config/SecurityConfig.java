2aedff37c81f31f2740dc90d18b913b92c9688d7
Eduardo Castro <eduardofc.dev@gmail.com>
2023-01-09 22:48:23 -0300

    Configuração de segurança para antigo padrão

---
+++ src/main/java/com/efc/helpdesk/config/SecurityConfig.java
@@ -0,0 +1,69 @@
+package com.efc.helpdesk.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.efc.helpdesk.security.JWTAuthenticationFilter;
+import com.efc.helpdesk.security.JWTUtil;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	private static final String[] PUBLIC_MATCHERS = {"/h2-console/**"}; // Libera acesso ao H2
+	
+	@Autowired
+	private Environment env;
+	@Autowired
+	private JWTUtil jwtUtil;
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		if(Arrays.asList(env.getActiveProfiles()).contains("test")) { // Configuração para perfil de 'test'
+			http.headers().frameOptions().disable();
+		}
+		
+		http.cors().and()
+			.csrf().disable(); // Desabilita proteção ataque CSRF pois o sistema não armazena sessão de usuário 
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil)); // Filtro de autenticação
+		http.authorizeRequests()
+			.antMatchers(PUBLIC_MATCHERS).permitAll()  // Permite URL definida na váriavel
+			.anyRequest().authenticated();	
+		http.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS); // Não permite criação de sessão para usuários 
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+	}
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues(); // Libera métodos padrão (GET, HEAD, POST)
+		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(); // Configuração de Cors
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder(); // Decodificação de senhas no DB
+	}
+	
+}
