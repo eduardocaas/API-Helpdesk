47f3e6e046daa6802c421b88c11df6dd5aaabae3
Eduardo Castro <104233554+eduardocaas@users.noreply.github.com>
2023-01-20 23:17:14 -0300

    teste bugfix

--- src/main/java/com/efc/helpdesk/services/TecnicoService.java
+++ src/main/java/com/efc/helpdesk/services/TecnicoService.java
@@ -54,8 +54,7 @@ public class TecnicoService {
 		objDTO.setId(id);
 		Tecnico oldObj = findById(id); // Verifica de id existe
 		
-		if(!objDTO.getSenha().equals(oldObj.getSenha()))  // se a senhar não for a mesma da anterior, faz encode
-			objDTO.setSenha(encoder.encode(objDTO.getSenha()));
+		
 		
 		validaPorCpfEEmail(objDTO); 
 		oldObj = new Tecnico(objDTO);
